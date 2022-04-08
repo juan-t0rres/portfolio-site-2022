b4bbdbb13808cbb4af05223eeea6059de7e5949a
juan-t0rres <juanditb@gmail.com>
2022-04-08 14:43:12 +0000

    footer changes

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,6 +1,6 @@
 function Page(props) {
   return (
-    <div className="page" style={props.style}>
+    <div style={{ ...props.style, paddingBottom: 10 }}>
       <h2>{props.title}</h2>
       <div style={{ marginTop: -10 }}>{props.children}</div>
     </div>
